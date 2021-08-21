8e830c59ffac5b4aeef4f599cbc297cc0cdb5c51
luis <gamaerry@gmail.com>
2021-08-20 20:26:43 -0500

    (mas) layout_translate.fxml terminado

---
+++ src/main/java/ControladorTranslate.java
@@ -0,0 +1,19 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class ControladorTranslate {
+    @FXML
+    private Button translate;
+    @FXML
+    private TextField escribe;
+    @FXML
+    private Label traduccion;
+    @FXML
+    private RadioButton espanol;
+    @FXML
+    private RadioButton english;
+
+}
