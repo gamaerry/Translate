752fc517b9f9576ffba6efcb83ad59d99cb5e0a4
luis <gamaerry@gmail.com>
2021-08-23 01:52:58 -0500

    jar attribute y icono agregados

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.util.Objects;
 public class Principal extends Application {
@@ -13,6 +14,7 @@ public class Principal extends Application {
         escenario.setTitle(datos[1]);
         escenario.setScene(
                 new Scene(raiz, Integer.parseInt(datos[2]), Integer.parseInt(datos[3])));
+        escenario.getIcons().add(new Image("ico.png"));
         escenario.show();
     }
 }
